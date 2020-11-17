265008120073a67ed0b30d193c7f7faebe12a3e3
Vašek Potoček <vasek.potocek@post.cz>
2020-11-17 22:26:19 +0100

    Custom focus / blur rules in result of click / enter action

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
-let sendAction = e => e.target.dispatchEvent(new CustomEvent('action', { bubbles: true, cancelable: true }));
+let sendAction = e => {
+  e.target.dispatchEvent(new CustomEvent('action', { bubbles: true, cancelable: true }));
+  e.preventDefault();
+}
+
 window.addEventListener('mousedown', sendAction);
 window.addEventListener('keydown', e => { if(e.key === 'Enter') sendAction(e) });
 window.addEventListener('touchstart', sendAction);
+
+window.addEventListener('action', e => {
+  if(e.defaultPrevented)
+    return;
+  // A click outside the active element blurs it. Granting focus is needless for click and for touch,
+  // and keyboard navigation should not be affected. This behaviour can be disabled by e.preventDefault(),
+  // primarily when moving focus to a different element in result of an action.
+  if(!document.activeElement.contains(e.target))
+    document.activeElement.blur();
+});
