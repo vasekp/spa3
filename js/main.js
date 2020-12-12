735b924fd25939f50b073ed00249f9fcf045decd
Vašek Potoček <vasek.potocek@post.cz>
2020-12-12 23:42:58 +0100

    Restore click outside to blur behaviour

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 window.addEventListener('mousedown', e => e.preventDefault()); // click does not grant focus
+
+window.addEventListener('click', e => {
+  if(!e.defaultPrevented && document.activeElement)
+    document.activeElement.blur();
+  e.preventDefault();
+});
