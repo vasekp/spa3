a7d77e952a6143a52d6593615d7aeade02f5a121
Vašek Potoček <vasek.potocek@post.cz>
2020-12-20 00:03:00 +0100

    Flags: load data from assets

--- js/countries.js
+++ js/countries.js
@@ -1,2 +1,35 @@
 import './components/tri-state.js';
 import './components/spa-scroll.js';
+
+window.addEventListener('DOMContentLoaded', () => {
+  loadData();
+});
+
+async function loadData() {
+  const response = await fetch('assets/countries.csv');
+  const text = await response.text();
+  const tbody = document.getElementById('flg-list').tBodies[0];
+  const colors = {
+    'E': 'blue',
+    'AS': 'yellow',
+    'AF': 'black',
+    'AO': 'green',
+    'SA': 'red',
+    'JA': 'red'
+  };
+  for(const line of text.split('\n')) {
+    if(!line) break;
+    const arr = line.split(':');
+    const tr = document.createElement('tr');
+    const color = colors[arr[3]];
+    const content = arr[3] === 'SA' || arr[4] === 'JA' ? `data-content="${arr[3][0]}"` : "";
+    tr.innerHTML = `
+      <td><img src="assets/flags/${arr[5]}.svg"/></td>
+      <td>${arr[0]}</td>
+      <td>${arr[2]}</td>
+      <td><span class="patch c-${color}" ${content} data-color="param"></span></td>
+    `;
+    tbody.appendChild(tr);
+  }
+  document.querySelector('.spa-loading').hidden = true;
+}
