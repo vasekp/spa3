b7a69ac2bf7a412a94ecefc30803c5c63d049e3a
Vašek Potoček <vasek.potocek@post.cz>
2020-12-22 09:56:23 +0100

    Make <spa-checkbox> keyboard-accessible

--- js/components/spa-checkbox.js
+++ js/components/spa-checkbox.js
@@ -16,21 +16,26 @@ const next_3state = {
 export class CheckBoxElement extends HTMLElement {
   constructor() {
     super();
-    this.addEventListener('click', () => {
-      const sw = this.dataset.states === "3" ? next_3state : next_2state;
-      this.dataset.state = sw[this.dataset.state] || states.checked;
-      this.dispatchEvent(new CustomEvent('input', { bubbles: true }));
-      this.dispatchEvent(new CustomEvent('changed', { bubbles: true }));
-    });
+    this.addEventListener('click', () => this._toggle());
+    this.addEventListener('keydown', e => { if(e.key === ' ') this._toggle(); });
   }
 
   connectedCallback() {
     if(!this.dataset.state)
       this.dataset.state = this.dataset.states === "3" ? states.unset : states.checked;
+    if(!this.hasAttribute('tabindex'))
+      this.setAttribute('tabindex', 0);
   }
 
   get stateBool() {
     return this.dataset.state === states.unset ? null : this.dataset.state === states.checked;
+  }
+
+  _toggle() {
+    const sw = this.dataset.states === "3" ? next_3state : next_2state;
+    this.dataset.state = sw[this.dataset.state] || states.checked;
+    this.dispatchEvent(new CustomEvent('input', { bubbles: true }));
+    this.dispatchEvent(new CustomEvent('changed', { bubbles: true }));
   }
 }
 
