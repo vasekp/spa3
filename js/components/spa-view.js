a58700861d7a87fc41652ae196b3498f47aa9432
Vašek Potoček <vasek.potocek@post.cz>
2020-12-29 20:12:30 +0100

    Basic module menu

--- js/components/spa-view.js
+++ js/components/spa-view.js
@@ -3,9 +3,9 @@ template.innerHTML = `
 <link rel="stylesheet" type="text/css" href="css/components/spa-view.css"/>
 <style>:not(:defined) { display: none; }</style>
 <spa-slideout id="controls" class="corner">
-  <button part="move-icon"><img class="inline" src="images/move.svg"/></button>
-  <button><img class="inline" src="images/settings.svg"/></button>
-  <button><img class="inline" src="images/home.svg"/></button>
+  <button id="move" part="move-icon"><img class="inline" src="images/move.svg"/></button>
+  <button id="settings"><img class="inline" src="images/settings.svg"/></button>
+  <button id="home"><img class="inline" src="images/home.svg"/></button>
 </spa-slideout>
 <div id="content"></div>`;
 
@@ -14,6 +14,8 @@ export class ViewElement extends HTMLElement {
     super();
     this.attachShadow({mode: 'open'});
     this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.shadowRoot.getElementById('home').addEventListener('click',
+      () => this.dataset.module = "menu");
   }
 
   static get observedAttributes() {
@@ -21,24 +23,23 @@ export class ViewElement extends HTMLElement {
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
-    this.loadView(newValue);
+    if(newValue !== oldValue)
+      this.loadView(newValue);
   }
 
   async loadView(view) {
     const cont = this.shadowRoot.getElementById('content');
-    cont.innerHTML = '';
-    const loading = document.createElement('div');
-    loading.classList.add('spa-loading');
-    cont.appendChild(loading);
+    if(view !== "menu") // switching to menu should look fluid
+      cont.innerHTML = '<div class="spa-loading"></div>';
     const [response, script] = await Promise.all([
       fetch(`${view}.mod.html`).then(r => r.text()),
       import(`../${view}.js`),
       this.addStyleSheet('css/modules.css'),
       this.addStyleSheet(`css/${view}.css`)
     ]);
-    loading.insertAdjacentHTML('afterend', await response);
-    loading.remove();
+    cont.innerHTML = await response;
     script.init(this.shadowRoot);
+    document.dispatchEvent(new CustomEvent('view-change'));
   }
 
   async addStyleSheet(filename) {
---
+++ js/menu.js
@@ -0,0 +1,38 @@
+import './components/spa-scroll.js';
+
+export async function init(root) {
+  const items = await(await fetch('modules.json')).json();
+  const cont = root.getElementById('menu-container');
+  const cmp = new Intl.Collator().compare;
+  items.sort((a, b) => cmp(a.displayName, b.displayName));
+  for(const item of items) {
+    const div = document.createElement('div');
+    div.classList.add('menu-item');
+    div.textContent = item.displayName;
+    div.dataset.moduleName = item.moduleName;
+    div.tabindex = 0;
+    div.dataset.active = 1;
+    cont.appendChild(div);
+  }
+  const cb = () => filter(cont);
+  cb();
+  document.addEventListener('view-change', cb);
+  cont.addEventListener('click', e => {
+    document.removeEventListener('view-change', cb);
+    root.host.dataset.module = e.target.dataset.moduleName;
+  });
+}
+
+function filter(cont) {
+  let odd = true;
+  for(const item of cont.children) {
+    const modName = item.dataset.moduleName;
+    const view = document.querySelector(`spa-view[data-module="${modName}"`);
+    const used = view && getComputedStyle(view).display !== 'none';
+    item.hidden = used;
+    if(!used) {
+      item.classList.toggle('alt-row', odd);
+      odd = !odd;
+    }
+  }
+}
