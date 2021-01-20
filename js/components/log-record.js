vasekp/spa3

commit d05b09120903b2d79cae991f7c1d27505bda309c

Unify all focusout behaviour under <spa-focus-container> and deriveds

js/components/log-record.js
@@ -2,6 +2,7 @@ import {Enum} from '../util/enum.js';
 import {timeFormat} from '../util/datetime.js';
 import {recordStore} from '../log-record-store.js';
 import {lsKeys, getGameLabels} from '../logbook.js';
+import {ContainerElement} from './spa-focus-container.js';
 
 const templateBase = document.createElement('template');
 templateBase.innerHTML = `
@@ -25,7 +26,7 @@ templateBase.innerHTML = `
 
 const states = Enum.fromArray(['nascent', 'base', 'edit']);
 
-export class RecordElement extends HTMLElement {
+export class RecordElement extends ContainerElement {
   connectedCallback() {
     this._construct();
     if(!this.state)
@@ -48,13 +49,7 @@ export class RecordElement extends HTMLElement {
     id('geo-icon').addEventListener('click', () => this._geoShow());
     id('geo-button').addEventListener('click', e => this._geoSet());
     id('colorsel').addEventListener('color-click', e => this._colorsel(e));
-    this.addEventListener('focusout', e => {
-      if(!this.contains(e.relatedTarget))
-        this._close();
-    });
-    if(!this.hasAttribute('tabindex'))
-      this.setAttribute('tabindex', -1);
-    this.dataset.focusContainer = 1;
+    this.addEventListener('focus-leave', () => this._close());
     this._constructed = true
   }
 

js/components/log-game.js
@@ -3,6 +3,7 @@ import {Enum} from '../util/enum.js';
 import {dateFormat} from '../util/datetime.js';
 import {gameStore} from '../log-game-store.js';
 import {lsKeys, getLabelsGames} from '../logbook.js';
+import {ContainerElement} from './spa-focus-container.js';
 
 const template = document.createElement('template');
 template.innerHTML = `
@@ -20,7 +21,7 @@ template.innerHTML = `
 
 const states = Enum.fromArray(['nascent', 'disabled', 'base', 'edit', 'color', 'confirm']);
 
-export class GameRecordElement extends HTMLElement {
+export class GameRecordElement extends ContainerElement {
   connectedCallback() {
     this._construct();
     if(!this.state)
@@ -49,15 +50,10 @@ export class GameRecordElement extends HTMLElement {
     id('color-sel').addEventListener('color-click', e => { this._colorClicked(e.detail.color); e.preventDefault(); });
     id('tools').addEventListener('click', e => e.preventDefault());
     this.addEventListener('click', e => this._click(e));
-    if(!this.hasAttribute('tabindex'))
-      this.setAttribute('tabindex', 0);
-    this.dataset.focusContainer = 1;
+    this.setAttribute('tabindex', 0);
     this.dataset.active = 1;
     this.classList.add('inner-outline');
-    this.addEventListener('focusout', e => {
-      if(!this.contains(e.relatedTarget))
-        this._close();
-    });
+    this.addEventListener('focus-leave', () => this._close());
     this._constructed = true;
   }
 

js/components/spa-focus-container.js
@@ -1,0 +1,11 @@
+export class ContainerElement extends HTMLElement {
+  constructor() {
+    super();
+    this.addEventListener('focusout', e => {
+      if(!this.contains(e.relatedTarget))
+        this.dispatchEvent(new CustomEvent('focus-leave'));
+    });
+  }
+}
+
+window.customElements.define('spa-focus-container', ContainerElement);

js/components/spa-modal.js
@@ -1,24 +1,35 @@
 import './spa-scroll.js';
+import {ContainerElement} from './spa-focus-container.js';
 
 const template = document.createElement('template');
 template.innerHTML = `
 <link rel="stylesheet" type="text/css" href="css/components/spa-modal.css"/>
-<spa-scroll id="scroll"><div id="content" tabindex="-1" data-focus-container="1" class="no-outline"><slot></slot></div></spa-scroll>`;
+<spa-scroll id="scroll"><div id="content"><slot></slot></div></spa-scroll>`;
 
-export class ModalElement extends HTMLElement {
+export class ModalElement extends ContainerElement {
   constructor() {
     super();
     this.attachShadow({mode: 'open'});
     this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.addEventListener('focus-leave', () => this.hide());
+    this.addEventListener('click', e => {
+      if(e.target === this)
+        this.hide();
+    });
   }
 
-  static get observedAttributes() {
-    return ['hidden'];
+  connectedCallback() {
+    if(!this.hasAttribute('tabindex'))
+      this.setAttribute('tabindex', -1);
   }
 
-  attributeChangedCallback(name, oldValue, value) {
-    if(!value)
-      this.shadowRoot.getElementById('content').focus();
+  show() {
+    this.hidden = false;
+    this.focus();
+  }
+
+  hide() {
+    this.hidden = true;
   }
 }
 

js/components/spa-view.js
@@ -14,7 +14,7 @@ template.innerHTML = `
 </spa-slideout>
 <div id="content"></div>
 <spa-modal id="settings-modal" hidden>
-  <div class="settings" tabindex="-1" data-focus-container="1">
+  <div class="settings" tabindex="-1">
     <div class="trans" id="module-settings-container"></div>
     <div class="trans" id="shared-settings-container"></div>
   </div>
@@ -36,11 +36,7 @@ export class ViewElement extends HTMLElement {
       s2.innerHTML = '';
       if(this.script.populateSettings)
         this.script.populateSettings(s2);
-      settings.hidden = false;
-    });
-    settings.addEventListener('focusout', e => {
-      if(!settings.contains(e.relatedTarget))
-        settings.hidden = true;
+      settings.show();
     });
     root.getElementById('move').addEventListener('click',
       () => alert('Ikonku zkuste táhnout a pustit do jiného panelu.'));

js/countries.js
@@ -214,6 +214,5 @@ function flagClicked(e) {
   root.getElementById('flg-d-name').textContent = tr.dataset.name;
   root.getElementById('flg-d-capital').textContent = tr.dataset.capital;
   root.getElementById('flg-d-currency').textContent = `${tr.dataset.currency} (${tr.dataset.abbrCurr})`;
-  root.getElementById('flg-details-modal').hidden = false;
-  root.getElementById('flg-details-modal').focus();
+  root.getElementById('flg-details-modal').show();
 }

js/main.js
@@ -42,7 +42,7 @@ window.addEventListener('keydown', e => {
     if(e.key === 'Enter' || e.key === ' ')
       target.dispatchEvent(new MouseEvent('click', { bubbles: true }));
   if(target.tagName === 'INPUT' && target.type === 'text' && e.key === 'Enter') {
-    const cont = target.closest('[data-focus-container]');
+    const cont = target.closest('spa-focus-container, spa-modal');
     if(cont)
       cont.focus();
     else

commit 56d331216dfd209427d6f13ba341dc03fd730761

Focus: fix a regression from d05b091

js/components/log-record.js
@@ -50,6 +50,7 @@ class RecordElement extends ContainerElement {
     id('geo-icon').addEventListener('click', () => this._geoShow());
     id('geo-button').addEventListener('click', e => this._geoSet());
     id('colorsel').addEventListener('color-click', e => this._colorsel(e));
+    this.setAttribute('tabindex', -1);
     this.addEventListener('focus-leave', () => this._close());
     this._constructed = true
   }