a42a665d6a915f4dd7c976588ec9c95a8e858b2b
Vašek Potoček <vasek.potocek@post.cz>
2021-01-26 10:16:27 +0100

    Wordlist: gradual loading of results

--- js/wordlist.js
+++ js/wordlist.js
@@ -5,10 +5,17 @@ import normalize from './util/text.js';
 import debounce from './util/debounce.js';
 import _, * as i18n from './i18n.js';
 
+const BASE = 200, MORE = 100;
+
 export default function(root) {
   const list = root.getElementById('list');
   const linesP = loadFile('assets/any/wordlists/cs-subst.txt');
   const reLowerCase = /^\p{Ll}/u;
+  const io = new IntersectionObserver(entries => {
+    if(entries.some(e => e.intersectionRatio > 0))
+      loadMore();
+  });
+  let flines = null;
 
   {
     const from = root.getElementById('lcount-from');
@@ -40,23 +47,63 @@ export default function(root) {
   }
 
   async function filter(f) {
-    console.log(f);
     const lines = await linesP;
     console.time('filter');
     let c = 0;
-    const flines = (function*() {
+    flines = (function*() {
       for(const line of lines)
         if(f(line))
           yield line;
     })();
+    io.disconnect();
+    list.scrollIntoView();
+    if(list.children.length > BASE)
+      clearFrom(BASE);
+    let cnt = 0;
     for(const li of list.children) {
       const {value, done} = flines.next();
-      if(done)
-        li.textContent = '';
-      else
+      if(done) {
+        clearFrom(cnt);
+        flines = null;
+        break;
+      } else {
         li.textContent = value;
+        ++cnt;
+      }
     }
+    loadMore(BASE - list.children.length, false);
     console.timeEnd('filter');
+  }
+
+  function clearFrom(ix) {
+    const rng = document.createRange();
+    rng.setStart(list, ix);
+    rng.setEnd(list, list.childNodes.length);
+    rng.deleteContents();
+    console.log(list.children.length);
+  }
+
+  function loadMore(upTo = MORE, scrolling = true) {
+    if(!flines)
+      return;
+    io.disconnect();
+    if(scrolling && list.lastChild)
+      io.observe(list.lastChild);
+    for(let i = 0; i < upTo; i++) {
+      const {value, done} = flines.next();
+      if(done) {
+        io.disconnect();
+        flines = null;
+        break;
+      } else {
+        const li = document.createElement('li');
+        li.textContent = value;
+        list.appendChild(li);
+      }
+    }
+    if(list.lastChild)
+      io.observe(list.lastChild);
+    console.log(list.children.length);
   }
 
   function append(f, test) {
