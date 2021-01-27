c8e5057a2b42771b811759c88a67790e71abd948
Vašek Potoček <vasek.potocek@post.cz>
2021-01-28 00:20:22 +0100

    <spa-dropdown>: fix position:fixed relative to contain:paint elements

--- js/components/spa-dropdown.js
+++ js/components/spa-dropdown.js
@@ -5,7 +5,7 @@ template.innerHTML = `
 <link rel="stylesheet" type="text/css" href="css/components/spa-dropdown.css"/>
 <div id="ref"><span id="header" data-active="1"><slot name="header"></slot><button id="arrow" class="no-outline"></button></span>
 <div id="ext" hidden><slot></slot></div>
-</div><span id="strut">&#x200B;</span>`;
+</div><span id="strut">&#x200B;</span><div id="origin"></div>`;
 
 class DropDownElement extends ContainerElement {
   constructor() {
@@ -20,8 +20,9 @@ class DropDownElement extends ContainerElement {
       this.dispatchEvent(new CustomEvent('pause-scroll', { detail: { pause: open }, bubbles: true }));
       if(open) {
         const rect = ref.getBoundingClientRect();
-        ref.style.left = `${rect.x}px`;
-        ref.style.top = `${rect.y}px`;
+        const orect = root.getElementById('origin').getBoundingClientRect();
+        ref.style.left = `${rect.x - orect.x}px`;
+        ref.style.top = `${rect.y - orect.y}px`;
       } else
         ref.style.position = '';
       root.getElementById('ext').hidden = !open;
