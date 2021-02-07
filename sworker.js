f6e31358c0e1ce5bad7066c232a65ff4020e9636
Vašek Potoček <vasek.potocek@post.cz>
2021-02-07 22:26:41 +0100

    PWA: catch fetch() errors

--- sworker.js
+++ sworker.js
@@ -5,7 +5,7 @@ self.addEventListener('activate', e => e.waitUntil(clients.claim()));
 self.addEventListener('fetch', e => {
   const url = new URL(e.request.url);
   if(url.origin === location.origin)
-    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)).catch(() => null));
   else
     e.respondWith(fetch(e.request));
 });
