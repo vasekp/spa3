fb012d935377502a4b074408abd2060bfe9ee8ec
Vašek Potoček <vasek.potocek@post.cz>
2021-02-27 22:55:08 +0100

    Codec: configurable decoders

--- js/codec.js
+++ js/codec.js
@@ -12,16 +12,16 @@ export { Err as DecodeError };
 export const braille = (() => {
   const letters = [1, 3, 9, 25, 17, 11, 27, 19, 10, 26, 5, 7, 13, 29, 21, 15, 31, 23, 14, 30, 37, 39, 55, 45, 61, 53];
 
-  return function(c, ch) {
+  return () => function(c, ch) {
     return latinIfGT0(letters.indexOf(c)) || new Err(ch);
-  }
+  };
 })();
 
 export const morse = (() => {
   const letters = [12, 2111, 2121, 211, 1, 1121, 221, 1111, 11, 1222, 212, 1211, 22, 21, 222, 1221, 2212, 121, 111, 2, 112, 1112, 122, 2112, 2122, 2211];
   const digits = [22222, 12222, 11222, 11122, 11112, 11111, 21111, 22111, 22211, 22221];
 
-  return function morse(c, ch, state) {
+  return () => function(c, ch, state) {
     if(c === 2 || c === null) {
       if(!state.x)
         return ' ';
@@ -39,42 +39,72 @@ export const morse = (() => {
       state.in = (state.in || '') + ch;
       return '';
     }
-  }
+  };
 })();
 
-export function pigpen1(c, ch) {
-  return c < 26 ? latin(c + 1) : new Err(ch);
+export const pigpen1 = conf => {
+  return conf['pigpen1'] === 'cz'
+    ? (c, ch) => c === 8 ? 'Ch' : c < 8 ? latin(c + 1) : latin(c)
+    : (c, ch) => c < 26 ? latin(c + 1) : ' ';
+};
+
+export const pigpen2 = conf => {
+  const xyz = conf['pigpen2'] === 'dot-9'
+    ? (xy, z) => xy * 3 + z
+    : (xy, z) => z * 9 + xy;
+  const p1 = pigpen1(conf);
+  return function(c, ch) {
+    const [xy, z] = [Math.trunc(c / 3), c % 3];
+    return p1(xyz(xy, z));
+  }
 }
 
-export function pigpen2(c, ch) {
-  const [xy, z] = [c >> 1, c & 1];
-  const [x, y] = xy < 9 ? [0, xy] : [1, xy - 9];
-  const a = x * 18 + z * (x ? 4 : 9) + y;
-  return a < 26 ? latin(a + 1) : new Err(ch);
-}
+export const pigpen3 = (() => {
+  const funcs = {
+    '9-dot-4': (x, y, z) => x * 18 + z * (x ? 4 : 9) + y,
+    '9-4-dot': (x, y, z) => z * 13 + x * 9 + y,
+    'dot-9-4': (x, y, z) => x * 18 + y * 2 + z
+  };
 
-export function pigpen3(c, ch) {
-  const [xy, z] = [Math.trunc(c / 3), c % 3];
-  const a = z * 9 + xy;
-  return a < 26 ? latin(a + 1) : new Err(ch);
-}
+  return conf => {
+    const xyz = funcs[conf['pigpen3']];
+    if(!xyz)
+      throw new Error(`Asked for a nonexistent pigpen/3 variant '${conf['pigpen3']}'!`);
 
-export function polybius(c, ch) {
-  return c < 16 ? latin(c + 1) : c < 25 ? latin(c + 2) : new Err(ch);
-}
+    return function(c, ch) {
+      const [xy, z] = [c >> 1, c & 1];
+      const [x, y] = xy < 9 ? [0, xy] : [1, xy - 9];
+      const a = xyz(x, y, z);
+      return a < 26 ? latin(a + 1) : new Err(ch);
+    };
+  };
+})();
+
+export const polybius = conf => {
+  switch(conf['polybius']) {
+    case 'q':
+      return (c, ch) => c < 16 ? latin(c + 1) : c < 25 ? latin(c + 2) : new Err(ch);
+    case 'j':
+      return (c, ch) => c < 9 ? latin(c + 1) : c < 25 ? latin(c + 2) : new Err(ch);
+    case 'k':
+      return (c, ch) => c < 10 ? latin(c + 1) : c < 25 ? latin(c + 2) : new Err(ch);
+    default:
+      throw new Error(`Asked for a nonexistent Polybius variant '${conf['polybius']}'!`);
+  };
+};
 
 export const segments = (() => {
   const digits = [0b0111111, 0b0000110, 0b1011011, 0b1001111, 0b1100110, 0b1101101, 0b1111101, 0b0000111, 0b1111111, 0b1101111];
   const letters = [0b1110111, 0b1111100, 0b1011000, 0b1011110, 0b1111001, 0b1110001, 0b0111101, 0b1110100, 0b0110000, 0b0011110, 0b1110101, 0b0111000, 0b0110111, 0b1010100, 0b1011100, 0b1110011, 0b1100111, 0b1010000, 0b1101101, 0b1111000, 0b0011100, 0b0111110, 0b1111110, 0b1110110, 0b1101110, 0b1011011]; /* S = 5, Z = 2 */
 
-  return function(c, ch) {
+  return () => function(c, ch) {
     return digitIfGT0(digits.indexOf(c))
       || latinIfGT0(letters.indexOf(c))
       || new Err(ch);
-  }
+  };
 })();
 
-export function flags(c, ch) {
+export const flags = () => function(c, ch) {
   if(c >= 1 && c <= 26)
     return latin(c);
   else if(c >= 32 && c < 42)
@@ -83,12 +113,12 @@ export function flags(c, ch) {
     return digit(c - 48);
   else
     return new Err(ch);
-}
+};
 
 export const semaphore = (() => {
   const letters = [1, 2, 3, 4, 5, 6, 7, 10, 11, 38, 12, 13, 14, 15, 19, 20, 21, 22, 23, 28, 29, 39, 46, 47, 30, 55];
 
-  return function(c, ch) {
+  return () => function(c, ch) {
     return latinIfGT0(letters.indexOf(c)) || new Err(ch);
   }
 })();
@@ -99,8 +129,8 @@ const map = [
   [0xF008, morse],
   [0xF00B, null],
   [0xF100, pigpen1],
-  [0xF120, pigpen2],
-  [0xF140, pigpen3],
+  [0xF120, pigpen3],
+  [0xF140, pigpen2],
   [0xF160, polybius],
   [0xF180, segments],
   [0xF200, null],
@@ -126,15 +156,16 @@ function category(c) {
   return [];
 }
 
-export function* decode(str) {
+export function* decode(str, conf) {
   let lastFunc = null;
+  let decoder = null;
   let state = {};
   for(const ch of str) {
     const c = ch.codePointAt(0);
     const [func, base] = category(c);
     if(func !== lastFunc) {
-      if(lastFunc && Object.keys(state).length > 0)
-        yield lastFunc(null, null, state);
+      if(decoder && Object.keys(state).length > 0)
+        yield decoder(null, null, state);
       state = {};
     }
     if(!func) {
@@ -142,10 +173,11 @@ export function* decode(str) {
       yield ch;
     } else {
       lastFunc = func;
-      const ret = func(c - base, ch, state);
+      decoder = func(conf);
+      const ret = decoder(c - base, ch, state);
       yield ret !== null ? ret : new Err(ch);
     }
   }
-  if(lastFunc && Object.keys(state).length > 0)
-    yield lastFunc(null, null, state);
+  if(decoder && Object.keys(state).length > 0)
+    yield decoder(null, null, state);
 }
--- js/decoder.js
+++ js/decoder.js
@@ -1,24 +1,58 @@
 import './components/spa-scroll.js';
 import './components/spa-textbox.js';
 import {decode, DecodeError} from './codec.js';
+import Enum from './util/enum.js';
 import * as i18n from './i18n.js';
+
+const lsKeys = Enum.fromObj({
+  pigpen1: 'dec-pp1',
+  pigpen2: 'dec-pp2',
+  pigpen3: 'dec-pp3',
+  polybius: 'dec-polybius',
+});
+
+const defaults = Enum.fromObj({
+  pigpen1: 'cz',
+  pigpen2: '9-dot',
+  pigpen3: '9-dot-4',
+  polybius: 'q'
+});
+
+const keys = Object.keys(defaults);
 
 export default function(root) {
   const inp = root.getElementById('in');
   const out = root.getElementById('out');
 
-  inp.addEventListener('input', e => {
+  const conf = [];
+  for(const key of keys)
+    conf[key] = localStorage[lsKeys[key]] || defaults[key];
+
+  function update() {
     substMorse(inp.area);
     if(out.replaceChildren)
-      out.replaceChildren(...markErrors(decode(inp.value)))
+      out.replaceChildren(...markErrors(decode(inp.value, conf)))
     else {
       while(out.lastChild)
         out.removeChild(out.lastChild);
-      out.append(...markErrors(decode(inp.value)));
+      out.append(...markErrors(decode(inp.value, conf)));
     }
-  });
+  }
 
-  return {};
+  inp.addEventListener('input', update);
+
+  function populateSettings(cont) {
+    cont.append(root.getElementById('module-settings').content.cloneNode(true));
+    for(const key of keys)
+      cont.querySelector(`input[name="${key}"][data-value="${conf[key]}"]`).checked = true;
+    cont.querySelector(`#dcd-set-defaults`).addEventListener('input', e => {
+      localStorage[lsKeys[e.target.name]] = e.target.dataset.value;
+      conf[e.target.name] = e.target.dataset.value;
+      update();
+    });
+  }
+
+  return { populateSettings };
 }
 
 function* markErrors(iter) {
