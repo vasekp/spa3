64d591b9c46387b51f7345a842e66dfc26c045cb
Vašek Potoček <vasek.potocek@post.cz>
2022-01-15 15:19:48 +0100

    Titlebar/preparation: passive odd/even rows in 'list' (no more dynamic entry hiding)

--- js/list.js
+++ js/list.js
@@ -2,21 +2,6 @@ import './components/spa-scroll.js';
 import * as i18n from './i18n.js';
 
 export default function(root) {
-  /*function filter() {
-    let odd = true;
-    const cont = root.getElementById('list');
-    for(const item of cont.children) {
-      const modName = item.dataset.moduleName;
-      const view = document.querySelector(`spa-view[data-module="${modName}"]:not([data-size=""])`);
-      const used = !!view;
-      item.hidden = used;
-      if(!used) {
-        item.classList.toggle('alt-row', odd);
-        odd = !odd;
-      }
-    }
-  }*/
-
   (async () => {
     const items = await(await fetch(`trans/${i18n.lang}/modules.json`)).json();
     const cont = root.getElementById('list');
@@ -30,11 +15,7 @@ export default function(root) {
       div.dataset.active = 1;
       cont.appendChild(div);
     }
-    //filter();
-    //document.addEventListener('view-change', filter);
     cont.addEventListener('click', e => {
-      //document.removeEventListener('view-change', filter);
-      //root.host.dataset.module = e.target.dataset.moduleName;
       root.dispatchEvent(new CustomEvent('request-module',
         { detail: { module: e.target.dataset.moduleName }, // view ID added in bubbling
           bubbles: true, composed: true }));
